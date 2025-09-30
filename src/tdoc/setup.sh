50b9917b1f965fa638ac2442d91a6803c75519e8
Adam Pollock <adamp@monsterrg.com>
2025-09-30 12:03:24 -0400

    fix

--- src/tdoc/setup.sh
+++ src/tdoc/setup.sh
@@ -1,7 +1,7 @@
 (echo '#!/usr/bin/env deno run --no-check -A'; deno bundle mod.ts) > exe.js
 chmod +x exe.js
 FILE_IN_BIN=$(which run-tests)
+BIN_DIR=$(dirname "$FILE_IN_BIN")
 FULL_PATH="$BIN_DIR/tdoc"
-BIN_DIR="/Users/adam/.local/bin"
 echo "Linking $FULL_PATH"
 cp ./exe.js "$FULL_PATH"
